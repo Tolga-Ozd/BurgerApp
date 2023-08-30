5ce28e0d6c74d0a8e6eb83aee2492c648ed12a50
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-08-30 18:06:32 +0300

    app.css

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import {Data} from "../helpers/Data"
 
+
 const Menu = () => {
-  
+  // console.log(Data)
   return (
-    <div>
-      
+    <div className='menuDiv'> 
+      {
+      Data.map(({name, image,content,price})=>(
+        <div className='cardMenu'>
+        <img src={image} alt=""  />
+        <h3 >{name}</h3>
+        <p>{content}</p>
+        <h5 >{price}</h5>
+      </div>
+      ))}
     </div>
   )
 }
--- src/helpers/Data.js
+++ src/helpers/Data.js
@@ -1,5 +1,4 @@
 
-import BurgerImage from   "../assets/burger.jpg";
 
 export const Data = [
     {
@@ -10,25 +9,25 @@ export const Data = [
     },
     {
         name:" King Burger",
-        image: BurgerImage,
+        image: 'https://www.burgerhome.com.tr/images/luiurun.jpg',
         content : "150 gr kasap Burger , patates , turşu ve kola",
         price: 250 , 
     },
     {
         name:"Köfte Burger",
-        image: "https://www.summahealth.org/-/media/project/summahealth/website/page-content/flourish/2_18a_fl_fastfood_400x400.webp?la=en&h=400&w=400&hash=145DC0CF6234A159261389F18A36742A",
+        image:'https://images.deliveryhero.io/image/fd-tr/LH/loji-hero.jpg',
         content : "180 gr Köfte Burger , patates , kola",
         price: 230 , 
     },
     {
         name:"Efso Burger",
-        image: BurgerImage,
+        image: 'https://www.burgerhome.com.tr/images/luiurun.jpg',
         content : "150 gr kasap Burger , patates , turşu",
         price: 250 , 
     },
     {
         name:"Köfte Burger",
-        image: "https://www.summahealth.org/-/media/project/summahealth/website/page-content/flourish/2_18a_fl_fastfood_400x400.webp?la=en&h=400&w=400&hash=145DC0CF6234A159261389F18A36742A",
+        image: 'https://images.deliveryhero.io/image/fd-tr/LH/loji-hero.jpg',
         content : "180 gr Köfte Burger , patates , kola",
         price: 230 , 
     },
